7b02371ea0cb1f2ec091a4075a72d64a61d86f03
Aspectzxcc <josh10nathan@gmail.com>
2024-06-20 20:19:30 +0800

    improved comments for client and server files

--- src/client.c
+++ src/client.c
@@ -1,7 +1,7 @@
 #include <winsock2.h>
 #include <stdio.h>
 
-#pragma comment(lib, "ws2_32.lib") // Link with ws2_32.lib
+#pragma comment(lib, "ws2_32.lib") // link with Windows Socket Library
 
 int main(int argc, char *argv[]) {
     WSADATA wsaData;
@@ -10,37 +10,37 @@ int main(int argc, char *argv[]) {
     char *message, server_reply[2000];
     int recv_size;
 
-    // Initialize Winsock
-    WSAStartup(MAKEWORD(2,2), &wsaData);
+    // initialize winsock
+    WSAStartup(MAKEWORD(2,2), &wsaData); // request version 2.2 of winsock
 
-    // Create socket
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    // create socket
+    s = socket(AF_INET, SOCK_STREAM, 0); // create a socket for the IPv4 address family using TCP
 
-    // Setup the server structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_port = htons(12345);
+    // setup the server structure
+    server.sin_family = AF_INET; // set address family to IPv4
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // set server address
+    server.sin_port = htons(12345); // set server port, converting from host to network byte order
 
-    // Connect to remote server
+    // connect to remote server
     if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        puts("connect error");
+        puts("connect error"); // if connection fails, print error
         return 1;
     }
 
-    puts("Connected");
+    puts("connected"); // print success message
 
-    // Receive a reply from the server
+    // receive a reply from the server
     if((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR) {
-        puts("recv failed");
+        puts("recv failed"); // if receiving fails, print error
     }
 
-    puts("Reply received\n");
-    server_reply[recv_size] = '\0';
-    puts(server_reply);
+    puts("reply received\n"); // print success message
+    server_reply[recv_size] = '\0'; // null-terminate the received data
+    puts(server_reply); // print the server reply
 
-    // Cleanup
-    closesocket(s);
-    WSACleanup();
+    // cleanup
+    closesocket(s); // close the socket
+    WSACleanup(); // clean up winsock
 
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,7 +1,7 @@
 #include <winsock2.h>
 #include <stdio.h>
 
-#pragma comment(lib, "ws2_32.lib") // link with ws2_32.lib
+#pragma comment(lib, "ws2_32.lib") // link with Windows Socket Library
 
 int main() {
     WSADATA wsaData;
@@ -9,40 +9,40 @@ int main() {
     struct sockaddr_in server, client;
     int c;
 
-    // init winsock library
-    WSAStartup(MAKEWORD(2,2), &wsaData);
+    // initialize winsock library for use
+    WSAStartup(MAKEWORD(2,2), &wsaData); // request version 2.2 of Winsock on the system
 
-    // create socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // create a new socket for communication
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0); // specify the IPv4 address family, TCP type socket
 
-    // prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(12345);
+    // set up the sockaddr_in structure for the server
+    server.sin_family = AF_INET; // use IPv4 addresses
+    server.sin_addr.s_addr = INADDR_ANY; // accept connections to all the IPs of the machine
+    server.sin_port = htons(12345); // specify port number, converting host byte order to network byte order
 
-    // bind the socket
+    // bind the socket to the local IP address and port
     bind(serverSocket, (struct sockaddr *)&server, sizeof(server));
 
-    // listen for incoming connections
-    listen(serverSocket, 3);
+    // listen on the socket for incoming connections
+    listen(serverSocket, 3); // set the maximum number of queued connections
 
-    // accept incoming connection
-    puts("Waiting for incoming connections...");
+    // wait for incoming connections
+    puts("waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
-    clientSocket = accept(serverSocket, (struct sockaddr *)&client, &c);
+    clientSocket = accept(serverSocket, (struct sockaddr *)&client, &c); // accept a connection
     if (clientSocket == INVALID_SOCKET) {
-        printf("accept failed with error code : %d", WSAGetLastError());
+        printf("accept failed with error code : %d", WSAGetLastError()); // if failed, print error
     }
 
-    puts("Connection accepted");
+    puts("connection accepted");
 
-    // communicate with client
-    char *message = "Hello Client";
-    send(clientSocket, message, strlen(message), 0);
+    // send a welcome message to the client
+    char *message = "hello client";
+    send(clientSocket, message, strlen(message), 0); // send the message to the client
 
-    // disconnect and cleanup
+    // close the socket and clean up
     closesocket(serverSocket);
-    WSACleanup();
+    WSACleanup(); // clean up Winsock
 
     return 0;
 }
