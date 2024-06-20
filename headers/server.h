6ff63c90e0ca8e8830088eae532c8ac4954b1f01
Aspectzxcc <josh10nathan@gmail.com>
2024-06-21 01:59:58 +0800

    added success messages to server header and removed it from client header

--- headers/server.h
+++ headers/server.h
@@ -8,6 +8,13 @@
 // link with winsock library
 #pragma comment(lib, "ws2_32.lib")
 
+// define message success responses
+#define MESSAGE_SUCCESSFUL_CONNECTION "Connection to the File Exchange Server is successful!"
+#define MESSAGE_SUCCESSFUL_DISCONNECTION "Connection closed. Thank you!"
+#define MESSAGE_SUCCESSFUL_REGISTRATION "Welcome %s!" // %s is the user's handle or alias
+#define MESSAGE_SUCCESSFUL_FILE_UPLOAD "%s<%s>: Uploaded %s" // %s and %s is the user's handle or alias, timestamp, and filename respectively
+#define MESSAGE_SUCCESSFUL_DIR_LIST "Server Directory\n%s" // %s is the directory listing
+
 // function prototypes
 DWORD WINAPI client_handler(void* data);
 
