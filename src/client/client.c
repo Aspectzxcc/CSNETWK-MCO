3253d24cb64ede278a24c1aae4237330b0b3923f
Aspectzxcc <josh10nathan@gmail.com>
2024-06-21 02:38:31 +0800

    added connection error message on failed socket connection in the function to init socket connection

--- src/client/client.c
+++ src/client/client.c
@@ -24,7 +24,7 @@ void initSocketConnection(SOCKET *sock, WSADATA *wsaData, SOCKADDR_IN *server, c
 
     // Connect to remote server
     if (connect(*sock, (struct sockaddr *)server, sizeof(*server)) < 0) {
-        fprintf(stderr, "Connect error");
+        fprintf(stderr, ERROR_CONNECTION_FAILED "\n");
         closesocket(*sock);
         WSACleanup();
         *sock = INVALID_SOCKET;
